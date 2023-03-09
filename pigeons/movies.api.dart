0e90a3f7ab417d60ebaced91bb449453e5d6d07c
Ahmed <ahmed.hussein@zarcony.dev>
2023-03-09 18:11:10 +0200

    - Update MoviesHoseApi file, by convert getMovies to return Object? to return json and make it async function for api call in native side

--- pigeons/movies.api.dart
+++ pigeons/movies.api.dart
@@ -1,11 +1,11 @@
+// Generate the Pigeon code for Dart, Android and iOS.
+// This will create the `MoviesHostApi` class in Android and iOS.
+// You can run this code with the generate_pigeon.sh script.
+// See https://pub.dev/packages/pigeon for more information.
 import 'package:pigeon/pigeon.dart';
-
-class Movie {
-  String? title;
-  String? date;
-}
 
 @HostApi()
 abstract class MoviesHostApi {
-  List<Movie?> getMovies(int pageNumber);
+  @async
+  Object? getMovies(String apiKey);
 }
