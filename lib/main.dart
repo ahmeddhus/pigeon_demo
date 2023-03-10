db70636015d5c79e9b5a518df4fcda1b72ec3c6b
Ahmed <ahmed.hussein@zarcony.dev>
2023-03-10 15:35:19 +0200

    - Disable debugPrint in release mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instabug_flutter_task/home_page.dart';
 
 void main() {
+
+  // This is used to disable the debugPrint() function in release mode.
+  if (kReleaseMode) debugPrint = (String? message, {int? wrapWidth}) {};
+
   runApp(const MyApp());
 }
 
