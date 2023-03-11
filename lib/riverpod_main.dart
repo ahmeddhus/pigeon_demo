9233b2d7c32185e76715921da537907b85bb3748
Ahmed <ahmed.hussein@zarcony.dev>
2023-03-11 05:39:22 +0200

    - Add main entry point to run app that use riverpod as state management

--- lib/riverpod_main.dart
+++ lib/riverpod_main.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:instabug_flutter_task/app.dart';
 
+//Run the app with the riverpod flavor
+//That use the riverpod package for state management
 void main({bool useRiverpod = false}) {
   // This is used to disable the debugPrint() function in release mode.
   if (kReleaseMode) debugPrint = (String? message, {int? wrapWidth}) {};
 
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(
+        useRiverpod: true,
+      ),
+    ),
+  );
 }
-
