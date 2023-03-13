3f6faf737a044f07d444e6a5b083d64cddc9bb40
Ahmed <ahmed.hussein@zarcony.dev>
2023-03-13 11:38:55 +0200

    - Add unit tests for fromMap class

---
+++ test/from_map/from_map_mocks.dart
@@ -0,0 +1,27 @@
+mixin FromMapMocks{
+  static Map<String, dynamic> fromMapMock = <String, dynamic>{
+    "string": "sdgsgs",
+    "bool1": true,
+    "bool2": false,
+    "bool3": "true",
+    "bool4": "false",
+    "bool5": "0",
+    "bool6": "1",
+    "bool7": null,
+    "int1": 232,
+    "int2": "345",
+    "int3": 234.33,
+    "int4": "234.33",
+    "int5": "345g",
+    "int6": null,
+    "double1": 232,
+    "double2": "345",
+    "double3": 234.33,
+    "double4": "234.33",
+    "double5": "345g",
+    "double6": null,
+    "double7": "233,13.2123",
+    "double8": "245,233,13.2",
+  };
+
+}
---
+++ test/from_map/from_map_test.dart
@@ -0,0 +1,95 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:instabug_flutter_task/utils/from_map.dart';
+
+import 'from_map_mocks.dart';
+
+void main() {
+  test('FromMap Mixin test', () async {
+    final FromMap converter = FromMap(map: FromMapMocks.fromMapMock);
+    dynamic result;
+    result = converter.convertToString(key: "string");
+    expect(result, "sdgsgs");
+
+    result = converter.convertToDouble(key: "double1");
+    expect(result, const TypeMatcher<double>());
+
+    result = converter.convertToDouble(key: "double2");
+    expect(result, const TypeMatcher<double>());
+
+    result = converter.convertToDouble(key: "double3");
+    expect(result, const TypeMatcher<double>());
+
+    result = converter.convertToDouble(key: "double4");
+    expect(result, const TypeMatcher<double>());
+
+    result = converter.convertToDouble(key: "double5", defaultValue: 202);
+    expect(result, const TypeMatcher<double>());
+
+    result = converter.convertToDouble(key: "double6");
+    expect(result, null);
+
+    result = converter.convertToDouble(key: "double7", defaultValue: 202);
+    expect(result, 23313.2123);
+
+    result = converter.convertToDouble(key: "double8", defaultValue: 202);
+    expect(result, 24523313.2);
+
+    result = converter.convertToInt(key: "int1");
+    expect(result, const TypeMatcher<int>());
+
+    result = converter.convertToInt(key: "int2");
+    expect(result, const TypeMatcher<int>());
+
+    result = converter.convertToInt(key: "int3");
+    expect(result, null);
+
+    result = converter.convertToInt(key: "int4");
+    expect(result, null);
+
+    result = converter.convertToInt(key: "int5", defaultValue: 202);
+    expect(result, const TypeMatcher<int>());
+
+    result = converter.convertToInt(key: "int6");
+    expect(result, null);
+
+    result = converter.convertToBool(key: "bool6");
+    expect(result, true);
+
+    result = converter.convertToBool(key: "bool7");
+    expect(result, null);
+
+    result = converter.convertToBool(key: "bool7", defaultValue: true);
+    expect(result, true);
+
+    result = converter.convertToBool(key: "bool1");
+    expect(result, true);
+
+    result = converter.convertToBool(key: "bool2");
+    expect(result, false);
+
+    result = converter.convertToBool(key: "bool3");
+    expect(result, true);
+
+    result = converter.convertToBool(key: "bool4");
+    expect(result, false);
+
+    result = converter.convertToBool(key: "bool5");
+    expect(result, false);
+
+    result = converter.convertToBool(key: "bool6");
+    expect(result, true);
+
+    result = converter.convertToBool(key: "bool7");
+    expect(result, null);
+
+    result = converter.convertToBool(key: "bool7", defaultValue: true);
+    expect(result, true);
+  });
+}
