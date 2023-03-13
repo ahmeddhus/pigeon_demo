8754a91528d4bba979e900a507ba233221370695
Ahmed <ahmed.hussein@zarcony.dev>
2023-03-13 11:47:29 +0200

    - Separate from_map tests

--- test/from_map/from_map_test.dart
+++ test/from_map/from_map_test.dart
@@ -11,85 +11,119 @@ import 'package:instabug_flutter_task/utils/from_map.dart';
 import 'from_map_mocks.dart';
 
 void main() {
-  test('FromMap Mixin test', () async {
-    final FromMap converter = FromMap(map: FromMapMocks.fromMapMock);
-    dynamic result;
-    result = converter.convertToString(key: "string");
-    expect(result, "sdgsgs");
+  group('FromMap Mixin test', () {
+    late FromMap converter;
 
-    result = converter.convertToDouble(key: "double1");
-    expect(result, const TypeMatcher<double>());
+    setUpAll(() {
+      converter = FromMap(map: FromMapMocks.fromMapMock);
+    });
 
-    result = converter.convertToDouble(key: "double2");
-    expect(result, const TypeMatcher<double>());
+    test('Test String conversion', () async {
+      dynamic result;
 
-    result = converter.convertToDouble(key: "double3");
-    expect(result, const TypeMatcher<double>());
+      // Test String conversion
+      result = converter.convertToString(key: "string1");
+      expect(result, const TypeMatcher<String>());
+      expect(result, "sdgsgs");
 
-    result = converter.convertToDouble(key: "double4");
-    expect(result, const TypeMatcher<double>());
+      result = converter.convertToString(key: "string2");
+      expect(result, const TypeMatcher<String>());
+      expect(result, "123");
 
-    result = converter.convertToDouble(key: "double5", defaultValue: 202);
-    expect(result, const TypeMatcher<double>());
+      result = converter.convertToString(key: "string3");
+      expect(result, null);
+    });
 
-    result = converter.convertToDouble(key: "double6");
-    expect(result, null);
+    test('FromMap Mixin test', () async {
+      dynamic result;
 
-    result = converter.convertToDouble(key: "double7", defaultValue: 202);
-    expect(result, 23313.2123);
+      // Test double conversion
+      result = converter.convertToDouble(key: "double1");
+      expect(result, const TypeMatcher<double>());
 
-    result = converter.convertToDouble(key: "double8", defaultValue: 202);
-    expect(result, 24523313.2);
+      result = converter.convertToDouble(key: "double2");
+      expect(result, const TypeMatcher<double>());
 
-    result = converter.convertToInt(key: "int1");
-    expect(result, const TypeMatcher<int>());
+      result = converter.convertToDouble(key: "double3");
+      expect(result, const TypeMatcher<double>());
 
-    result = converter.convertToInt(key: "int2");
-    expect(result, const TypeMatcher<int>());
+      result = converter.convertToDouble(key: "double4");
+      expect(result, const TypeMatcher<double>());
 
-    result = converter.convertToInt(key: "int3");
-    expect(result, null);
+      result = converter.convertToDouble(key: "double5", defaultValue: 202);
+      expect(result, const TypeMatcher<double>());
 
-    result = converter.convertToInt(key: "int4");
-    expect(result, null);
+      result = converter.convertToDouble(key: "double5");
+      expect(result, null);
 
-    result = converter.convertToInt(key: "int5", defaultValue: 202);
-    expect(result, const TypeMatcher<int>());
 
-    result = converter.convertToInt(key: "int6");
-    expect(result, null);
+      result = converter.convertToDouble(key: "double6");
+      expect(result, null);
 
-    result = converter.convertToBool(key: "bool6");
-    expect(result, true);
+      result = converter.convertToDouble(key: "double7", defaultValue: 202);
+      expect(result, 23313.2123);
 
-    result = converter.convertToBool(key: "bool7");
-    expect(result, null);
+      result = converter.convertToDouble(key: "double8", defaultValue: 202);
+      expect(result, 24523313.2);
+    });
 
-    result = converter.convertToBool(key: "bool7", defaultValue: true);
-    expect(result, true);
+    test('FromMap Mixin test', () async {
+      dynamic result;
 
-    result = converter.convertToBool(key: "bool1");
-    expect(result, true);
+      result = converter.convertToInt(key: "int1");
+      expect(result, const TypeMatcher<int>());
 
-    result = converter.convertToBool(key: "bool2");
-    expect(result, false);
+      result = converter.convertToInt(key: "int2");
+      expect(result, const TypeMatcher<int>());
 
-    result = converter.convertToBool(key: "bool3");
-    expect(result, true);
+      result = converter.convertToInt(key: "int3");
+      expect(result, null);
 
-    result = converter.convertToBool(key: "bool4");
-    expect(result, false);
+      result = converter.convertToInt(key: "int4");
+      expect(result, null);
 
-    result = converter.convertToBool(key: "bool5");
-    expect(result, false);
+      result = converter.convertToInt(key: "int5", defaultValue: 202);
+      expect(result, const TypeMatcher<int>());
 
-    result = converter.convertToBool(key: "bool6");
-    expect(result, true);
+      result = converter.convertToInt(key: "int6");
+      expect(result, null);
+    });
 
-    result = converter.convertToBool(key: "bool7");
-    expect(result, null);
+    test('FromMap Mixin test', () async {
+      dynamic result;
 
-    result = converter.convertToBool(key: "bool7", defaultValue: true);
-    expect(result, true);
+      result = converter.convertToBool(key: "bool6");
+      expect(result, true);
+
+      result = converter.convertToBool(key: "bool7");
+      expect(result, null);
+
+      result = converter.convertToBool(key: "bool7", defaultValue: true);
+      expect(result, true);
+
+      result = converter.convertToBool(key: "bool1");
+      expect(result, true);
+
+      result = converter.convertToBool(key: "bool2");
+      expect(result, false);
+
+      result = converter.convertToBool(key: "bool3");
+      expect(result, true);
+
+      result = converter.convertToBool(key: "bool4");
+      expect(result, false);
+
+      result = converter.convertToBool(key: "bool5");
+      expect(result, false);
+
+      result = converter.convertToBool(key: "bool6");
+      expect(result, true);
+
+      result = converter.convertToBool(key: "bool7");
+      expect(result, null);
+
+      result = converter.convertToBool(key: "bool7", defaultValue: true);
+      expect(result, true);
+    });
   });
 }
